25eccec761dac8e10e00e039d6a100ce14ee64ba
Federico <federico.fiorini29@gmail.com>
2015-11-28 18:33:45 +0100

    views added

--- src/TradeYourGoods/WebBundle/Resources/views/Login/index.html.php
+++ src/TradeYourGoods/WebBundle/Resources/views/Login/index.html.php
@@ -2,4 +2,41 @@
 
 ?>
 
-LOGIN
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Login</title>
+
+    <!-- Bootstrap core CSS -->
+    <!--<link href="public/css/bootstrap.min.css" rel="stylesheet">-->
+    <link rel="stylesheet" type="text/css" href="<?php echo $view['assets']->getUrl('bundles/tradeyourgoodsweb/css/bootstrap.min.css') ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo $view['assets']->getUrl('bundles/tradeyourgoodsweb/css/signin.css') ?>">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <div class="container">
+      <form class="form-signin" action="/login" method="POST">
+        <h2 class="form-signin-heading">Login</h2>
+        <label for="mobile" class="sr-only">Phone number</label>
+        <input type="text" id="mobile" name="mobile" class="form-control" placeholder="Phone number" required autofocus>
+        <label for="password" class="sr-only">Password</label>
+        <input type="password" id="password" name="password" class="form-control" placeholder="Password" required>
+        <input class="btn btn-lg btn-primary btn-block" type="submit" value="Go">
+      </form>
+
+    </div>
+  </body>
+</html>
--- src/TradeYourGoods/WebBundle/Resources/views/Login/register.html.php
+++ src/TradeYourGoods/WebBundle/Resources/views/Login/register.html.php
@@ -2,4 +2,50 @@
 
 ?>
 
-REGISTER
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Registration</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" type="text/css" href="<?php echo $view['assets']->getUrl('bundles/tradeyourgoodsweb/css/bootstrap.min.css') ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo $view['assets']->getUrl('bundles/tradeyourgoodsweb/css/signin.css') ?>">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <!-- <script src="assets/js/ie-emulation-modes-warning.js"></script> -->
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <div class="container">
+
+      <form class="form-signin" action="/register" method="POST">
+        <h2 class="form-signin-heading">Register</h2>
+        <input type="hidden" name="mobile" value="<?php echo $mobile; ?>">
+        <label for="name" class="sr-only">Name</label>
+        <input type="text" id="name" class="form-control" placeholder="Name" required>
+        <label for="surname" class="sr-only">Surname</label>
+        <input type="text" id="surname" class="form-control" placeholder="Surname" required>
+        <label for="email" class="sr-only">Email address (optional)</label>
+        <input type="email" id="email" class="form-control" placeholder="Email address" required autofocus>
+        <label for="second_password" class="sr-only">Password</label>
+        <input type="second_password" id="second_password" class="form-control" placeholder="Type your password again" required>
+        <a class="btn btn-lg btn-primary btn-block" type="submit">Register now</a>
+      </form>
+
+    </div>
+  </body>
+</html>
